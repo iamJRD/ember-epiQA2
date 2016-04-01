90b15467dfa144ed0d996c98a2543dac9498d7e1
Jared Beckler <jared.beckler@gmail.com>
2016-04-01 08:22:41 -0700

    add ability to enter a question into the database using a form

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('question');
+  },
+
+  actions: {
+    saveQuestion3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+  }
 });
