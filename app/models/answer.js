5f9862d74af1153c3ea4891907db68c109dafd00
Jared Beckler <jared.beckler@gmail.com>
2016-04-02 22:23:50 -0700

    add commented sections for adding up and down votes for an answer. WIP as I am a little confused on the functionality of it all with Firebase

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr(),
   author: DS.attr(),
-  question: DS.belongsTo('question', {async: true})
+  question: DS.belongsTo('question', {async: true}),
+  // upVote: DS.attr(),
+  // downVote: DS.attr()
 });
--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,6 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'epi-qa',
     environment: environment,
-    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
     firebase: 'https://epi-qa.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
@@ -18,6 +17,16 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval' https://*.firebaseio.com",
+      'font-src': "'self'",
+      'connect-src': "'self' wss://*.firebaseio.com",
+      'img-src': "'self' *",
+      'style-src': "'self' 'unsafe-inline'",
+      'frame-src': "'self' https://*.firebaseio.com"
     }
   };
 
