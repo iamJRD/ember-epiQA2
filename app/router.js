69c7c619b4cd4f13930a9e2fca09ef0217cd7fb9
Jared Beckler <jared.beckler@gmail.com>
2016-04-08 14:43:26 -0700

    add about page, link to it, and styling

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('question', {path: '/question/:question_id'});
   this.route('bookmarks');
+  this.route('about');
 });
 
 export default Router;
